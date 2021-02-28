a6e4b6a75541417d962731b89d59190f5b78a028
wansiLiangg <caliangwansi@gmail.com>
2021-02-28 09:23:48 -0500

    Added inputs and outputs to GetClusterLabels and TrainModel

---
+++ GetClusterLabels.py
@@ -0,0 +1,7 @@
+
+#Get superclass labels from the dataset
+#vi (reduced data instance), yi (superclass label)
+def getClusterLabels (vi, yi):
+
+    return Zi
+	#Zi - a data structure containing all the data under the superclass label yi
---
+++ trainModel.py
@@ -0,0 +1,7 @@
+
+#Train final model to minimize maximum per-cluster risk
+#Input: data (sorted), cluster labels
+def trainModel (sortedData, cluster):
+
+	return newFeaturizer, linearClassificationHead
+    # parameterized featurizer & logit(training score/accuracy)
