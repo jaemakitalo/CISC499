19ed0b1714bb26f9dbffd586b9921d69a54c0360
wansiLiangg <caliangwansi@gmail.com>
2021-03-15 18:18:38 -0400

    Updated GetData

--- GetData.py
+++ GetData.py
@@ -6,26 +6,20 @@ Use the same Python list manipulation to obtain specific values from the data
 Examples:
     data = getData("analcatdata_aids")
     print(data.loc[0])  #Read the first row of the data
-    print(data.loc[0][:-1]) #Read the first row of the data without the superclass label
+    print(data.loc[0][:-1]) #Read the first row of the data without the superclass label -- method 1
+    print(pd.DataFrame(data.loc[i][:-1]))   #Read the first row of the data without the superclass label -- method 2
 '''
 
 import pandas as pd
 import os
-import requests
 
 #Get data of a specific dataset
 def getData(datasetName):
-    url = "https://github.com/EpistasisLab/pmlb/raw/master/datasets"
-    extension = ".tsv.gz"
-    datasetURL = getURL(url, datasetName, extension)
-    data = pd.read_csv(datasetURL, sep = "\t", compression = "gzip")
+    datasetPath = os.path.join("datasets", datasetName + ".tsv")
+    try:
+        data = pd.read_csv(datasetPath, sep = "\t")
+    except:
+        raise FileNotFoundError("Invalid dataset name")
     return data
 
-#Get the URL of the dataset from the PMLB Github
-def getURL(url, datasetName, extension):
-    datasetURL = os.path.join(url, datasetName, datasetName + extension)
-    requestURL = requests.get(datasetURL)
-    #Check if the provided datasetName is valid
-    if requestURL.status_code != 200:
-        raise ValueError('The provided dataset name is invalid')
-    return datasetURL
+print(getData("analcatdata_aids"))
