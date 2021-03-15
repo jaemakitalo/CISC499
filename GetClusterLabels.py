2f86d51c1832d97b3842db1a65133ddac756bf38
wansiLiangg <caliangwansi@gmail.com>
2021-03-15 15:34:43 -0400

    Updated GetClusterLabels and GetFeatureVectors

--- GetClusterLabels.py
+++ GetClusterLabels.py
@@ -1,7 +1,11 @@
 
 #Get superclass labels from the dataset
 #vi (reduced data instance), yi (superclass label)
-def getClusterLabels (vi, yi):
-
-    return Zi
-	#Zi - a data structure containing all the data under the superclass label yi
+def getClusterLabels (vi, yi, zi):
+    if yi in zi.keys():
+        valueVector = zi.get(yi)
+        valueVector.append([vi])
+    else:
+        zi[yi] = [[vi]]
+    return zi
+	#zi - a data structure containing all the data under the superclass label yi
--- GetFeatureVectors.py
+++ GetFeatureVectors.py
@@ -1,14 +1,24 @@
 import GetClusterLabels as gcl
+import GetData as gd
+import pandas as pd
+
+'''
+The featureVectors will look like:
+    {superclassLabel1: [[...], [...], ... , [...]], superclassLabel2: [[...], [...], ... , [...]]}
+'''
 
 #cluster the features of each superclass within the dataset
     #loops through the dataset
 #calls getClusterLabels within the loop
 
 #data = reduced data instance
-#y = superclass label
-def getFeatureVectors(data, y):
-    featureVectors = []
-    for i in range(len(y)):
-        output = gcl.getClusterLabels(data[i], y[i])
-        featureVectors.append (output)
+def getFeatureVectors(datasetName):
+    data = gd.getData(datasetName)  #Modify this line after we have the dimensionality reducer
+    featureVectors = {}
+    for i in range(len(data)):
+        #data[i][:-1] -- data without superclass labels, data[i][-1] -- superclass labels
+        featureVectors = gcl.getClusterLabels(pd.DataFrame(data.loc[i][:-1]), data.loc[i][-1], featureVectors)
     return featureVectors
+
+
+print(getFeatureVectors('analcatdata_aids'))
