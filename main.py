f5bdb291c4b732a6bb77b7dca6310d3690679539
jaemakitalo <56208032+jaemakitalo@users.noreply.github.com>
2021-03-31 10:04:27 -0400

    Update main.py
    Comments on figures for poster in main

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def data_classification():
     trainedModel = CLA.rfc (xTrain, yTrain)
 
     #select important features
+    # should modify threshold to be a percentage or ratio, not set number
+    # can look at distribution of features for the dataset
     sfm = CLA.SelectFromModel(trainedModel, threshold=0.20)
     sfm.fit(xTrain, yTrain)
 
@@ -73,6 +75,7 @@ def data_classification():
 
 if __name__ == "__main__":
     dataset, ft = data_classification()
+    # pass through data just under chosen headers
     feat = []
     print("Clustering the data...")
     kmeans = clustering(n_clusters=4, random_state=0).fit(dataset)
@@ -80,3 +83,24 @@ if __name__ == "__main__":
     print(kmeans.labels_)
     print("Cluster centers: ")
     print(kmeans.cluster_centers_)
+
+'''
+figures:
+    feature importance distribution
+    clustering results
+    plot classification performance
+    testing accuracy
+
+Think about the intermediate steps on how to get to the final result
+Think about interesting observations of the final results:
+    why do we have this result, take different perspective to make
+    more figures to try and answer the why question 
+
+For poster, most of the info should be represented using figures and tables
+(have some flow chats for the methodoly and 2-3 for the results)
+Do not want entire results section by its own. Use subsections for 
+to represent the final results
+
+Keep audience in mind - tailor our material/presentation to their background
+
+'''
